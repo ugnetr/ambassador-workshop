bdd96a272b26af360bc1b6eb741c55c534c4d45e
Ugne Trakimaite <ugnet@wix.com>
2023-06-12 22:11:20 +0300

    Add a component test

--- packages/ambassador-app/src/pages/index.spec.tsx
+++ packages/ambassador-app/src/pages/index.spec.tsx
@@ -2,7 +2,20 @@ import React from "react";
 import { render } from "@testing-library/react";
 import { PageHeaderTestkit } from "wix-style-react/dist/testkit";
 import { testkit } from "@wix/yoshi-flow-bm/testkit";
+import { whenRequest } from "@wix/yoshi-flow-bm/testkit/http-client";
 import Index from "./index";
+import { commentsAPI } from "../api/comments.api";
+
+const mockComments = [
+  {
+    author: "Mark",
+    text: "Mock comment",
+  },
+  {
+    author: "Dan",
+    text: "Mock comment 2",
+  },
+];
 
 describe("index", () => {
   testkit.beforeAndAfter();
@@ -21,5 +34,21 @@ describe("index", () => {
     });
 
     expect(await pageHeaderTestkit.exists()).toBe(true);
+  });
+
+  it("renders initial products", async () => {
+    const { TestComponent } = testkit.getBMComponent(Index, {
+      mocks: [
+        whenRequest(commentsAPI)
+          .withData("")
+          .reply(200, () => mockComments),
+      ],
+    });
+
+    const { findAllByTestId } = render(<TestComponent />);
+
+    const comments = await findAllByTestId("comment");
+
+    expect(comments.length).toBe(2);
   });
 });
