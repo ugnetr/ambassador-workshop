d646fdced754ed0397a96178ae71c6c9f47d532b
Ugne Trakimaite <ugnet@wix.com>
2023-06-12 17:41:39 +0300

    Rename comments test

--- packages/ambassador-app/src/api/comments.api.test.ts
+++ packages/ambassador-app/src/api/comments.api.test.ts
@@ -1,26 +1,20 @@
-import { bootstrap } from "@wix/yoshi-flow-bm/test/serverless";
 import HttpClient from "yoshi-serverless-client";
+import { bootstrap } from "yoshi-serverless-testing/build/index";
 import { NodeWorkshopScalaApp } from "@wix/ambassador-node-workshop-scala-app/rpc";
 import { aComment } from "@wix/ambassador-node-workshop-scala-app/builders";
+import { commentsAPI } from "./comments.api"; // import your server function
 
-const { serverlessApp } = bootstrap();
+const serverlessApp = bootstrap();
 serverlessApp.beforeAndAfter();
-
 let client: HttpClient;
-
 beforeAll(async () => {
   client = new HttpClient({ baseUrl: serverlessApp.getUrl() });
 });
-
-test("should fetch and return comments", async () => {
+test("should fetch comments", async () => {
   const commentsStub =
     serverlessApp.ambassador.createStub(NodeWorkshopScalaApp);
   const mockComment = aComment().build();
   commentsStub.CommentsService().fetch.when("1234").resolve([mockComment]);
-
   const response = await client.request(fetch)();
-
-  console.log("AAAA: ", response);
-
   expect(response).toEqual([mockComment]);
 });
