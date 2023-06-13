dccc17817bde16ffb08446295c6333088a79a3f9
Ugne Trakimaite <ugnet@wix.com>
2023-06-13 08:52:17 +0300

    Passing comments test

--- packages/ambassador-app/src/api/comments.api.test.ts
+++ packages/ambassador-app/src/api/comments.api.test.ts
@@ -1,20 +1,21 @@
-import HttpClient from "yoshi-serverless-client";
-import { bootstrap } from "yoshi-serverless-testing/build/index";
+import { bootstrap } from "@wix/yoshi-flow-bm/test/serverless";
 import { NodeWorkshopScalaApp } from "@wix/ambassador-node-workshop-scala-app/rpc";
 import { aComment } from "@wix/ambassador-node-workshop-scala-app/builders";
-import { commentsAPI } from "./comments.api"; // import your server function
+import { fetch } from "./comments.api";
 
-const serverlessApp = bootstrap();
-serverlessApp.beforeAndAfter();
-let client: HttpClient;
-beforeAll(async () => {
-  client = new HttpClient({ baseUrl: serverlessApp.getUrl() });
-});
-test("should fetch comments", async () => {
-  const commentsStub =
-    serverlessApp.ambassador.createStub(NodeWorkshopScalaApp);
-  const mockComment = aComment().build();
-  commentsStub.CommentsService().fetch.when("1234").resolve([mockComment]);
-  const response = await client.request(fetch)();
-  expect(response).toEqual([mockComment]);
+const { httpClient, serverlessApp } = bootstrap();
+
+describe("Comments API", () => {
+  serverlessApp.beforeAndAfter();
+
+  test("should fetch comments", async () => {
+    const commentsStub =
+      serverlessApp.ambassador.createStub(NodeWorkshopScalaApp);
+    const mockComment = aComment().build();
+    commentsStub.CommentsService().fetch.when("1234").resolve([mockComment]);
+
+    const { data: comments } = await httpClient.request(fetch("1234"));
+
+    expect(comments).toEqual([mockComment]);
+  });
 });
