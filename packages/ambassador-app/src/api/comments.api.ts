f1c3892d67abb25e6121c1b94e9e1353363bd8ec
Ugne Trakimaite <ugnet@wix.com>
2023-06-13 09:36:30 +0300

    Add add comment method

--- packages/ambassador-app/src/api/comments.api.ts
+++ packages/ambassador-app/src/api/comments.api.ts
@@ -1,8 +1,20 @@
 import { method } from "@wix/yoshi-flow-bm/serverless";
-import { NodeWorkshopScalaApp } from "@wix/ambassador-node-workshop-scala-app/rpc";
+import {
+  Comment,
+  NodeWorkshopScalaApp,
+} from "@wix/ambassador-node-workshop-scala-app/rpc";
 
 export const fetch = method(async function (metaSiteId: string) {
   const commentsService = NodeWorkshopScalaApp().CommentsService();
 
   return commentsService(this.context.aspects).fetch(metaSiteId);
+});
+
+export const add = method(async function (
+  metaSiteId: string,
+  comment: Comment
+) {
+  const commentsService = NodeWorkshopScalaApp().CommentsService();
+
+  return commentsService(this.context.aspects).add(metaSiteId, comment);
 });
